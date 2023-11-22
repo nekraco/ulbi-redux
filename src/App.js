aafea0e32f1e029ec9792a3b38829f953a709ace
nekraco <UrtaevAndreyA@gmail.com>
2023-11-22 20:13:28 +0300

    created store, reducer, provider to index.js

--- src/App.js
+++ src/App.js
@@ -2,9 +2,30 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  let ob = {
+    a: 111,
+    b: 222,
+  }
+
+  let ob2 = {...ob, c: (ob.a + ob.b), a: ob.a - ob.b}
+
+
+  const f = (ob) => {
+    let ar = [];
+    for(let key in ob) {
+      ar.push(key + ' : ' + ob[key]);
+    }
+    return ar
+  }
+
+
+
   return (
     <div className="App">
-
+        this-App
+      <hr/>
+      {f(ob2).map(el => <div>{el}</div>)}
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,12 +2,34 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import {createStore} from "redux";
+import {Provider} from "react-redux";
+
+
+let InitialStare = {
+	cash: 0,
+}
+
+let myReducer = (state = InitialStare, action) => {
+	switch (action.type) {
+		case 'PLUS-CASH':
+			return {...state, cash: state.cash + action.cash};
+
+		case 'MINUS-CASH':
+			return {...state, cash: state.cash - action.cash};
+
+		default:
+			return state;
+	}
+
+}
+
+const st = createStore(myReducer);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
 
-    <App />
-  
+root.render(
+	  <Provider store={st}><App /></Provider>
 );
 
 
