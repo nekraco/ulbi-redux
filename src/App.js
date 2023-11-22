767de243c5630b863e8eca7d6b4611c4b8e17bac
nekraco <UrtaevAndreyA@gmail.com>
2023-11-22 21:19:59 +0300

    realization plus-minus cash

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-
 import './App.css';
 import {useDispatch, useSelector} from "react-redux";
 
 function App() {
 
-  const dispatch = useDispatch();
-  const myState = useSelector(function (s) {
-    return s.cash
-  });
-
-  console.log(myState)
+	const dispatch = useDispatch();
+	const myState = useSelector(function (s) {
+		return s.cash
+	});
 
 
+	function plus() {
+		dispatch({type: 'PLUS-CASH', cash: 100,});
+	}
 
-  return (
-    <div className="App">
-        this-App
-      <hr/>
+	function minus() {
+		dispatch({type: 'MINUS-CASH', cash: 90,});
+	}
 
-      <div/></div>
-  );
+
+	return (
+		<div className="App">
+			<h2>This-App</h2>
+			<hr/>
+			<hr/>
+
+			<div className={'myState'}>{myState}</div>
+			<div>
+				<button onClick={plus}>Пополнить счет</button>
+				<button onClick={minus}>Снять со счета</button>
+
+			</div>
+
+		</div>
+
+	);
 }
 
 export default App;
